a82dcd5c58ed52d110e4f4871e6222961845b69e
Volker Mische <volker.mische@gmail.com>
2020-02-07 17:11:38 +0100

    feat: make it work without temp file
    Keep everything in memory instead of writing it to a temporary file.

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,15 @@ use std::env;
 use std::fs::File;
 use std::io;
 
-use filecoin_proofs::{generate_piece_commitment, SectorSize, UnpaddedBytesAmount};
+use filecoin_proofs::constants::DefaultPieceHasher;
+use filecoin_proofs::fr32::write_padded;
+use filecoin_proofs::{
+    generate_piece_commitment, PaddedBytesAmount, SectorSize, UnpaddedBytesAmount,
+};
 use hex;
+use storage_proofs::pieces::generate_piece_commitment_bytes_from_source;
+
+use std::io::{Cursor, Seek, SeekFrom};
 
 // use a file as an io::Reader but pad out extra length at the end with zeros up
 // to the padded size
@@ -58,26 +65,43 @@ fn padded_size(size: u64) -> u64 {
     return u64::from(UnpaddedBytesAmount::from(SectorSize(1 << (logv + 1))));
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
     let file = File::open(filename).expect("Unable to open file");
     let file_size = file.metadata().unwrap().len();
     let padded_file_size = padded_size(file_size);
-    let pad_reader = PadReader {
+    let mut pad_reader = PadReader {
         file: file,
         fsize: usize::try_from(file_size).unwrap(),
         padsize: usize::try_from(padded_file_size).unwrap(),
         pos: 0,
     };
-    let info = generate_piece_commitment(pad_reader, UnpaddedBytesAmount(padded_file_size))
-        .expect("failed to generate piece commitment");
+
+    //// Old code
+    //let info = generate_piece_commitment(pad_reader, UnpaddedBytesAmount(padded_file_size))
+    //   .expect("failed to generate piece commitment");
+    //let commitment = info.commitment;
+    //let piece_size = info.size;
+
+    let mut data = Vec::new();
+    let mut temp_piece_file = Cursor::new(&mut data);
+    // send the source through the preprocessor, writing output to temp file
+    let piece_size =
+       UnpaddedBytesAmount(write_padded(&mut pad_reader, &mut temp_piece_file)? as u64);
+    temp_piece_file.seek(SeekFrom::Start(0))?;
+    let commitment = generate_piece_commitment_bytes_from_source::<DefaultPieceHasher>(
+       &mut temp_piece_file,
+       PaddedBytesAmount::from(piece_size).into(),
+    )?;
 
     print!(
         "{} Size: {:?}, Padded: {:?}, CommP {}\n",
         args[1],
-        info.size,
+        piece_size,
         padded_file_size,
-        hex::encode(info.commitment)
+        hex::encode(commitment)
     );
+
+    Ok(())
 }
